5db6380eccc301ff272fa09c3cc68d4fe48fd3fa
Stas Prohetamine <prohetamine@gmail.com>
2022-10-22 19:30:02 +0300

    save

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs    = require('fs')
+    , path  = require('path')
+    , end   = require('node-mermaid-extension/plugin/end')
+    , NME   = require('node-mermaid-extension')
+
+const Mermaid = NME({
+  port: 3333,
+  debug: false,
+  test: false
+})
+
+const date = new Date()
+
+const intercepter = ({ fetch, test }) => {
+  fetch.usePost('/events', ({ event, parseBase64, sendMessageSocket, nextPlugin }) => {
+
+    if (!event.isParsedEvent) {
+      const data = `---------------------------------\n\n${JSON.stringify(parseBase64(event.pureEvent))}\n\n`
+
+      fs.appendFileSync(path.join(__dirname, 'events', event.platform + '-' + date), data)
+    }
+
+    nextPlugin()
+  })
+}
+
+;(async () => {
+  const extension = await Mermaid.ready()
+
+  extension.use(intercepter)
+  extension.use(end)
+})()
---
+++ script.mm
@@ -0,0 +1,30 @@
+{
+  "chaturbateHttpEvent": [{
+    "fetch": {
+      "url": "http://localhost:3333/events",
+      "method": "POST",
+      "mode": "cors",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body": "{\"contextId\":\"({event.contextId})\",\"id\":({event.id}),\"hashId\":\"({event.hashId})\",\"platform\":\"({event.platform})\",\"modelUsername\":\"({event.modelUsername})\",\"pureEvent\":\"({event.pureEvent})\",\"isParsedEvent\":({event.isParsedEvent}),\"parseEvent\":{\"isModel\":({event.parseEvent.isModel}),\"isUser\":({event.parseEvent.isUser}),\"isAnon\":({event.parseEvent.isAnon}),\"isNotice\":({event.parseEvent.isNotice}),\"isToken\":({event.parseEvent.isToken}),\"isRemovedMessage\":({event.parseEvent.isRemovedMessage}),\"isDisconnect\":({event.parseEvent.isDisconnect}),\"isConnect\":({event.parseEvent.isConnect}),\"isRoomCount\":({event.parseEvent.isRoomCount}),\"tokenCount\":({event.parseEvent.tokenCount}),\"message\":\"({event.parseEvent.message})\",\"username\":\"({event.parseEvent.username})\",\"roomCount\":({event.parseEvent.roomCount}),\"user\":\"({event.parseEvent.user})\",\"model\":\"({event.parseEvent.model})\",\"notice\":\"({event.parseEvent.notice})\"}}"
+    },
+    "cancel": 15000,
+    "attemptsError": 3,
+    "attemptsSleepError": 5000
+  }],
+  "stripchatHttpEvent": [{
+    "fetch": {
+      "url": "http://localhost:3333/events",
+      "method": "POST",
+      "mode": "cors",
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "body": "{\"contextId\":\"({event.contextId})\",\"id\":({event.id}),\"hashId\":\"({event.hashId})\",\"platform\":\"({event.platform})\",\"modelUsername\":\"({event.modelUsername})\",\"pureEvent\":\"({event.pureEvent})\",\"isParsedEvent\":({event.isParsedEvent}),\"parseEvent\":{\"isModel\":({event.parseEvent.isModel}),\"isUser\":({event.parseEvent.isUser}),\"isAnon\":({event.parseEvent.isAnon}),\"isNotice\":({event.parseEvent.isNotice}),\"isToken\":({event.parseEvent.isToken}),\"isRemovedMessage\":({event.parseEvent.isRemovedMessage}),\"isDisconnect\":({event.parseEvent.isDisconnect}),\"isConnect\":({event.parseEvent.isConnect}),\"isRoomCount\":({event.parseEvent.isRoomCount}),\"tokenCount\":({event.parseEvent.tokenCount}),\"message\":\"({event.parseEvent.message})\",\"username\":\"({event.parseEvent.username})\",\"roomCount\":({event.parseEvent.roomCount}),\"user\":\"({event.parseEvent.user})\",\"model\":\"({event.parseEvent.model})\",\"notice\":\"({event.parseEvent.notice})\"}}"
+    },
+    "cancel": 15000,
+    "attemptsError": 3,
+    "attemptsSleepError": 5000
+  }]
+}
